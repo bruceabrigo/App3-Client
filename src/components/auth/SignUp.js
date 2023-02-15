a4f40ae8191eed050fdfa4c788dca35f16cd3645
Rita HC <ritahameerchauhan@gmail.com>
2023-02-15 11:17:02 -0500

    Setting up Sign-Up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -9,18 +9,19 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const SignUp = (props) => {
-	// constructor(props) {
-	// 	super(props)
-
-	// 	this.state = {
-	// 		email: '',
-	// 		password: '',
-	// 		passwordConfirmation: '',
-	// 	}
-	// }    
+	// SETTING UP USE STATE FOR EACH ITEM  
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
+    const [profilePicture, setProfilePicture] = useState('')
+    const [coverPicture, setCoverPicture] = useState('')
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [city, setCity] = useState('')
+    const [active, setactive] =  useState('')
+
+
+
 
     const navigate = useNavigate()
 
@@ -29,7 +30,9 @@ const SignUp = (props) => {
 
 		const { msgAlert, setUser } = props
 
-        const credentials = {email, password, passwordConfirmation}
+        const credentials = {email, password, passwordConfirmation, name, description, profilePicture,
+            coverPicture, city
+        }
 
 		signUp(credentials)
 			.then(() => signIn(credentials))
@@ -41,7 +44,8 @@ const SignUp = (props) => {
 					variant: 'success',
 				})
 			)
-			.then(() => navigate('/'))
+            // Then Navigate to a path
+			.then(() => navigate('/sign-in'))
 			.catch((error) => {
                 setEmail('')
                 setPassword('')
@@ -60,6 +64,32 @@ const SignUp = (props) => {
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
                 <h3>Sign Up</h3>
                 <Form onSubmit={onSignUp}>
+
+                <Form.Group controlId='name'>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            required
+                            type='name'
+                            name='name'
+                            value={name}
+                            placeholder='Enter name'
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId='description'>
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            required
+                            type='description'
+                            name='description'
+                            value={description}
+                            placeholder='Description yourself'
+                            onChange={e => setDescription(e.target.value)}
+                        />
+                    </Form.Group>
+
+
                     <Form.Group controlId='email'>
                         <Form.Label>Email address</Form.Label>
                         <Form.Control
@@ -71,6 +101,7 @@ const SignUp = (props) => {
                             onChange={e => setEmail(e.target.value)}
                         />
                     </Form.Group>
+
                     <Form.Group controlId='password'>
                         <Form.Label>Password</Form.Label>
                         <Form.Control
@@ -93,6 +124,40 @@ const SignUp = (props) => {
                             onChange={e => setPasswordConfirmation(e.target.value)}
                         />
                     </Form.Group>
+
+                    <Form.Group controlId='profilePicture'>
+                        <Form.Label>Profile Picture</Form.Label>
+                        <Form.Control
+                            type='profilePicture'
+                            name='profilePicture'
+                            value={profilePicture}
+                            placeholder='Add your photo'
+                            onChange={e => setProfilePicture(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId='coverPicture'>
+                        <Form.Label>Cover Picture</Form.Label>
+                        <Form.Control
+                            type='coverPicture'
+                            name='coverPicture'
+                            value={coverPicture}
+                            placeholder='Add your photo'
+                            onChange={e => setCoverPicture(e.target.value)}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId='city'>
+                        <Form.Label>City</Form.Label>
+                        <Form.Control
+                            type='city'
+                            name='city'
+                            value={city}
+                            placeholder='Enter your city'
+                            onChange={e => setCity(e.target.value)}
+                        />
+                    </Form.Group>
+
                     <Button variant='primary' type='submit'>
                         Submit
                     </Button>
