dda219600dec1e379c3de5ce1139a268f7274b1d
bruceabrigo <bruceabrigo@outlook.com>
2023-02-19 19:10:41 -0600

    Centered text on image in user cards

--- src/components/auth/SignOut.js
+++ src/components/auth/SignOut.js
@@ -4,7 +4,7 @@ import {Button, ButtonGroup} from 'react-bootstrap'
 
 import { signOut } from '../../api/auth'
 import messages from '../shared/AutoDismissAlert/messages'
-
+import './SignOut.scss'
 const SignOut = (props) => {
 	const { msgAlert, clearUser, user } = props
     console.log(props)
@@ -30,18 +30,24 @@ const SignOut = (props) => {
 
 	return (
 		<>
-            <div className='row'>
-                <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                    <h2>Are you sure you want to sign out?</h2>
-                    <small>We hate to see you go...</small><br/>
-                    <ButtonGroup>
-                        <Button variant='danger' onClick={onSignOut}>
-                            Sign Out
-                        </Button>
-                        <Button variant='warning' onClick={onCancel}>
-                            Cancel
-                        </Button>
-                    </ButtonGroup>
+            <div className='logout'>
+                <div className='logout-card'>
+                    <div className='left'>
+                        <h1>We hate to see you go!</h1>
+                        <p>Are you sure you want to leave paradise?</p>
+                    </div>
+                    <div className='right'>
+                        <h2>Are you sure you want to sign out?</h2>
+                        <small>We hate to see you go...</small><br/>
+                        <ButtonGroup>
+                            <Button variant='danger' onClick={onSignOut}>
+                                Sign Out
+                            </Button>
+                            <Button className='ml-2' variant='warning' onClick={onCancel}>
+                                Cancel
+                            </Button>
+                        </ButtonGroup>
+                    </div>
                 </div>
             </div>
 		</>
